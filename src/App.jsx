8ce959361e830aae14642bc5eef83770aa0a434d
hipetech <korota1kirill@gmail.com>
2023-11-02 17:33:41 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className={"w-[500px] h-6 bg-amber-500"}>
+      <h2>
+          hello
+      </h2>
+    </div>
+  );
+}
+
+export default App;
